1336fd992e98e1b7f007eb95adbd996baa3e6219
VardhiniT <134366869+VardhiniT@users.noreply.github.com>
2024-02-08 23:36:53 +0530

    Update app.py

--- app.py
+++ app.py
@@ -38,10 +38,7 @@ def predict_datapoint():
         results=predict_pipeline.predict(pred_df)
         print("after Prediction")
 
-        return render_template('home.html',results=results[0])
-    
+        return render_template('home.html',results=results[0])    
 
 if __name__=="__main__":
     app.run(host="0.0.0.0",port=8080)        
-
-
